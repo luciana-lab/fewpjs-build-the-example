356639e0e64b1b3d9dee618968462cbbd0b24252
Luciana Sugeta <lulusugeta@gmail.com>
2021-09-03 17:20:23 -0400

    Done.

--- main.js
+++ main.js
@@ -4,6 +4,42 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+// Add the .hidden class to the error modal in the HTML so it does not appear when the page first loads
+const errorModal = document.getElementById('modal');
+errorModal.classList.add('hidden');
+
+const like = document.querySelectorAll('.like');
+
+
+let colorStates = {
+  "red": "",
+  "": "red"
+}
+
+let glyphState = {
+  '♥': '♡',
+  '♡': '♥'
+}
+
+function heartCallBack(e) {
+  console.log(e)
+  const heartClick = e.target;
+  mimicServerCall("fakeUrl")
+    .then(function (serverMessage) {
+      heartClick.style.color = colorStates[heartClick.style.color]
+      heartClick.innerText = glyphState[heartClick.innerText]
+      console.log(like)
+    })
+    .catch(function () {
+      errorModal.classList.remove('hidden');
+      setTimeout(() => errorModal.classList.add('hidden'), 3000);
+    })
+}
+
+for (let glypth of like) {
+  console.log(glypth)
+  glypth.addEventListener("click", heartCallBack)
+}
 
 
 
@@ -11,9 +47,9 @@ const FULL_HEART = '♥'
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
       let isRandomFailure = Math.random() < .2
       if (isRandomFailure) {
         reject("Random server error. Try again.");
